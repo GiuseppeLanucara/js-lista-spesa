f224e9d5fc63f6be20fff16bd604cc8ea81578bd
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-10-13 19:56:31 +0200

    creata struttura html e implementato inizio algoritmo js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+Consigli:
+- Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+- Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+Buon lavoro a tutte e a tutti!!*/
+
+const listaSpesa = ["mele", "banane", "latte", "pane"];
+console.log(listaSpesa);
+
+let i = 0;
+const lista = document.getElementsByClassName("list");
+console.log(lista);
+
+while (i < listaSpesa) {
+    i++;
+}
